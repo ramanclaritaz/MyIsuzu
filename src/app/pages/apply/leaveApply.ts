38f8c7944adf6b3a514607e7fb668d23b1fb92b2
CLARITAZ\SUDALAI.MANICKAM <sudalai.manikam@claritaz.com>
2017-12-18 22:32:20 +0530

    leavechanges

--- src/app/pages/apply/leaveApply.ts
+++ src/app/pages/apply/leaveApply.ts
@@ -52,7 +52,7 @@ export class leaveApply {
     this.globalVar.goBack = 'dash';
     this.getAvailableLeave();
     this.getTypeOFLeave();
-    this.items = { toDatevisiblity: false, fromFulldayvisiblity: false, toFulldayVisibilty: false, fromMorningVisiblity: false, toMorningVisiblity: false, isFullday: 0 };
+    this.items = {};
 
   }
 
@@ -73,7 +73,7 @@ export class leaveApply {
   }
 
   getLeaveCode(id: number): void {
-
+    this.items = {};
     let data = { params: { id: id, userId: this.auth.userid } }
     this.leaveService.getLeaveCode(data).subscribe(
       res => {
@@ -82,6 +82,7 @@ export class leaveApply {
           this.items.toDatevisiblity = this.items.toFulldayVisibilty = this.items.toMorningVisiblity = false;
           this.items.fromFulldayvisiblity = this.items.fromMorningVisiblity = true;
           this.items.fromfullorhalfday = 0;
+          this.items.fromMorningDisabled = this.items.fromFulldayDisabled = true;
         }
         else if (this.selectedTypeOfLeave.id == 4) {
           this.items.toDatevisiblity = this.items.toFulldayVisibilty = this.items.toMorningVisiblity = this.items.fromMorningVisiblity = false;
@@ -100,7 +101,7 @@ export class leaveApply {
         else if (this.selectedTypeOfLeave.id == 5) {
           this.selectedLeaveCode = this.leaveCode[0];
           this.items.toFulldayVisibilty = this.items.fromFulldayvisiblity = this.items.fromMorningVisiblity = this.items.toMorningVisiblity = false;
-          this.items.toDatevisiblity=true;
+          this.items.toDatevisiblity = true;
         }
         // if (this.selectedTypeOfLeave.id == 5) {
         //   this.selectedLeaveCode = null;
@@ -148,15 +149,16 @@ export class leaveApply {
     if (this.selectedLeaveCode != undefined && this.selectedLeaveCode != null) {
       let halfday = { afternoon: [6, 8, 27, 33, 35, 37, 23], morning: [5, 26, 31, 32, 34, 36, 22] };
       let isMorning = halfday.morning.filter((val) => {
-        if (this.selectedLeaveCode.id == val)
+        if (this.selectedLeaveCode.id == val) {
           return true;
+        }
       });
       let isAfternoon = halfday.afternoon.filter((val) => {
         if (this.selectedLeaveCode.id == val)
           return true;
       });
-      if (isMorning || isAfternoon) {
-        if (isMorning)
+      if (isMorning.length > 0 || isAfternoon.length > 0) {
+        if (isMorning.length > 0)
           this.items.fromMorningorAfternoon = 1;
         else {
           this.items.fromMorningorAfternoon = 0;
@@ -166,9 +168,7 @@ export class leaveApply {
         this.items.fromMorningVisiblity = false;
         this.items.fromfullorhalfday = 1;
       }
-
-
-      if (this.selectedTypeOfLeave.id == 6 && this.selectedLeaveCode.id == 17) {
+      else if (this.selectedTypeOfLeave.id == 6 && this.selectedLeaveCode.id == 17) {
         console.log("1"); console.log(this.selectedLeaveCode);
         this.items.fromfullorhalfday = 1;
         this.items.toFullorHalf = 1;
@@ -181,7 +181,26 @@ export class leaveApply {
         this.selectedTypeOfLeave.isToFulldaydisable = false;
       }
     }
+  }
+  changed() {
 
-
+    if (this.selectedTypeOfLeave.id == 8 || this.selectedTypeOfLeave.id == 9) {
+      if (this.items.fromfullorhalfday == 1) {
+        this.items.toFullorHalf = 0;
+        this.items.toMorningorAfternoon = 1;
+        this.items.fromMorningorAfternoon = undefined;
+        this.items.fromMorningVisiblity = false;
+        this.items.toFulldayDisabled = this.items.toMorningDisabled = this.items.toMorningVisiblity = true;
+      }
+      else {
+        {
+          this.items.toFullorHalf = 1;
+          this.items.toMorningorAfternoon = undefined;
+          this.items.toMorningVisiblity = false;
+          this.items.fromMorningorAfternoon = 0;
+          this.items.toFulldayDisabled = this.items.toMorningDisabled = this.items.fromMorningVisiblity = true;
+        }
+      }
+    }
   }
 }
